dce042ce136d5549b29e6ef0617a5fed6e059a37
Rupesh <rupeshk@adobe.com>
2025-02-05 20:45:11 +1100

    placeholders-fetchKeys

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,8 +1,15 @@
+import fetchPlaceholders from '../../scripts/aem.js';
+
 export default function decorate(block) {
     const [quoteWrapper] = block.children;
   
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
     quoteWrapper.replaceChildren(blockquote);
+
+    // Fetch placeholders
+    const placeholders = fetchPlaceholders('quote-key');
+    const { key } = placeholders;
+    console.error(key);
   }
   
