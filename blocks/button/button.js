f647d9a6dbb6a5185b59d364d53587a8d8eea92a
Rupesh <rupeshk@adobe.com>
2025-02-06 19:26:04 +1100

    button-wknd

--- blocks/button/button.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-  console.error("Decorating button block");
-  const button = block.querySelector("button");
-  if (button) {
-    button.classList.add("custom-button");
-  }
-}
