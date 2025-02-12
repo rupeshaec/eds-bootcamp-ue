31ee10707204a4796bd5d1781e5b029f010a2931
Rupesh <rupeshk@adobe.com>
2025-02-13 00:26:48 +1100

    article-styles

--- blocks/article/article.js
+++ blocks/article/article.js
@@ -6,5 +6,29 @@ async function getArticles(link) {
 const payload = await getArticles('/related-articles.json');
   
 export default function decorate(block) {
-    console.warn(payload);
+  const [heading, container] = block.children;
+
+  const template = `
+  <div class="article-card">
+    <div class="article-img">
+      <img src="{img}" alt="{title}" class="article-card__image">
+    </div>
+    <h1 class="article-card__title">{title}</h1>
+    <p class="article-card__description">{description}</p>
+  </div>
+  `;
+
+  var innerHTML = '';
+  payload.data.forEach(element => {
+    console.warn(element);
+    innerHTML += template
+      .replace('{img}', element.Image)
+      .replace('{title}', element.Title)
+      .replace('{title}', element.Title)
+      .replace('{description}', element.Description);
+  });
+
+  console.warn(heading);
+  console.warn(container);
+  container.innerHTML = innerHTML;
 }
